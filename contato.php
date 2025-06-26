95e6b826fa76221326b9cfa499f0c352ae2fa609
Gustavo Ribeiro Serafim <gustavoserafim59@gmail.com>
2025-06-25 22:07:50 -0300

    ok

--- contato.php
+++ contato.php
@@ -67,28 +67,9 @@
 
 </style>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilo.css">
-    <title>Setupp Shopp</title>
-</head>
-
-<body>
-    <header class="cabecalho">
-        <img src="img/icone.jpg" alt="logo" class="logo">
-        <h1 class="text1">Setupp Shopp</h1>
-        <nav>
-            <a class="letra" href="index.php">Home</a>
-            <a class="letra" href="Setups.php">Setups</a>
-            <a class="letra" href="contato.php">contato</a>
-        </nav>
-    </header>
+<?php
+include "cabecalho.php"
+?>
 
     <div class="form-container">
 
